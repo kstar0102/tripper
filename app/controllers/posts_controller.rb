ce8629baac617c6a0787c208ab00ba18016844da
kstar0102 <seniordev52@gmail.com>
2020-03-11 12:52:54 +0900

    投稿の編集と更新機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,17 @@ class PostsController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to root_path
+  end
+
+
   private
   def post_params
     params.require(:post).permit(:content, :image)
