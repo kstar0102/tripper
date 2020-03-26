38b9e76968f7827860f3275632653052d159ce26
kstar0102 <seniordev52@gmail.com>
2020-03-26 15:00:48 +0900

    セレクターを使いユーザーの旅行費情報を保存

---
+++ db/migrate/20200326041432_create_travelexpenses.rb
@@ -0,0 +1,10 @@
+class CreateTravelexpenses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :travelexpenses do |t|
+      t.string :name, null: false
+      t.string :ancestry
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200325035004) do
+ActiveRecord::Schema.define(version: 20200326041432) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -22,10 +22,18 @@ ActiveRecord::Schema.define(version: 20200325035004) do
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
-    t.text     "image",      limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.text     "image",             limit: 65535
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.integer  "user_id"
+    t.integer  "travelexpenses_id"
+  end
+
+  create_table "travelexpenses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",       null: false
+    t.string   "ancestry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
