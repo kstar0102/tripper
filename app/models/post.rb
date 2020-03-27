d8f07f2fa9cf188d3986b29976fde370c6beab56
kstar0102 <seniordev52@gmail.com>
2020-03-27 17:39:13 +0900

    ①投稿一覧表示箇所のViewを整えた②imagesテーブルを作成（画像を複数アップできるように）

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ApplicationRecord
   mount_uploader :image,ImageUploader
   validates :content, presence: true
   belongs_to :user
-  has_many :comments  # commentsテーブルとのアソシエーション
+  has_many :comments 
+  has_many :images 
 
   def self.search(search)
     return Post.all unless search
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200326113051) do
+ActiveRecord::Schema.define(version: 20200327073153) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20200326113051) do
     t.datetime "updated_at",               null: false
     t.integer  "post_id"
     t.text     "text",       limit: 65535
+  end
+
+  create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "post_id"
+    t.string   "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_images_on_post_id", using: :btree
   end
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -51,4 +59,5 @@ ActiveRecord::Schema.define(version: 20200326113051) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "images", "posts"
 end
