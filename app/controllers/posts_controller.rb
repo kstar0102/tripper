ae0dbcc3163390bd5ef97a32f63ff90b82bab6bc
kstar0102 <seniordev52@gmail.com>
2020-04-07 23:09:25 +0900

    ユーザーicon修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -41,7 +41,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:content, :image, :country, :travelexpense).merge(user_id: current_user.id)
+    params.require(:post).permit(:name, :content, :image, :country, :travelexpense).merge(user_id: current_user.id)
   end
 
 end
