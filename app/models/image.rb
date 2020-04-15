0a4913edd026d46cb57a7fd7997d86320b6ed337
kstar0102 <seniordev52@gmail.com>
2020-04-15 19:27:32 +0900

    画像複数投稿準備（中間テーブル）

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ApplicationRecord
   mount_uploader :image_url, ImageUploader
-  belongs_to :post
-  validates :image_url, presence: true
+  belongs_to :post, optional: true
+  # validates :image_url, presence: true
 end
