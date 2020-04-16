08ef50d19dcb0767796be8499cbaf9003f41516b
kstar0102 <seniordev52@gmail.com>
2020-04-16 13:20:02 +0900

    不要なコメントアウト削除

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -1,85 +1,28 @@
-// $(document).on('turbolinks:load', ()=> {
-//   // 画像用のinputを生成する関数
-//   const buildFileField = (num)=> {
-//     const html = `<div data-index="${num}" class="js-file_group">
-//                     <input class="js-file" type="file"
-//                     name="product[images_attributes][${num}][src]"
-//                     id="product_images_attributes_${num}_src"><br>
-//                     <div class="js-remove">削除</div>
-//                   </div>`;
-//     return html;
-//   }
-//   // プレビュー用のimgタグを生成する関数
-//   const buildImg = (index, url)=> {
-//     const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
-//     return html;
-//   }
-
-//   // file_fieldのnameに動的なindexをつける為の配列
-//   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
-//   // 既に使われているindexを除外
-//   lastIndex = $('.js-file_group:last').data('index');
-//   fileIndex.splice(0, lastIndex);
-
-//   $('.hidden-destroy').hide();
-
-//   $('#image-box').on('change', '.js-file', function(e) {
-//     const targetIndex = $(this).parent().data('index');
-//     // ファイルのブラウザ上でのURLを取得する
-//     const file = e.target.files[0];
-//     const blobUrl = window.URL.createObjectURL(file);
-
-//     // 該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
-//     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
-//       img.setAttribute('src', blobUrl);
-//     } else {  // 新規画像追加の処理
-//       $('#previews').append(buildImg(targetIndex, blobUrl));
-//       // fileIndexの先頭の数字を使ってinputを作る
-//       $('#image-box').append(buildFileField(fileIndex[0]));
-//       fileIndex.shift();
-//       // 末尾の数に1足した数を追加する
-//       fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
-//     }
-//   });
-
-//   $('#image-box').on('click', '.js-remove', function() {
-//     const targetIndex = $(this).parent().data('index');
-//     // 該当indexを振られているチェックボックスを取得する
-//     const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
-//     // もしチェックボックスが存在すればチェックを入れる
-//     if (hiddenCheck) hiddenCheck.prop('checked', true);
-
-//     $(this).parent().remove();
-//     $(`img[data-index="${targetIndex}"]`).remove();
-//     // 画像入力欄が0個にならないようにしておく
-//     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
-//   });
-// });
 $(function(){
-  //DataTransferオブジェクトで、データを格納する箱を作る
+  //1
   var dataBox = new DataTransfer();
-  //querySelectorでfile_fieldを取得
+  //2
   var file_field = document.querySelector('input[type=file]')
-  //fileが選択された時に発火するイベント
+  //3
   $('#img-file').change(function(){
-    //選択したfileのオブジェクトをpropで取得
+    //4
     var files = $('input[type="file"]').prop('files')[0];
     $.each(this.files, function(i, file){
-      //FileReaderのreadAsDataURLで指定したFileオブジェクトを読み込む
+      //5
       var fileReader = new FileReader();
 
-      //DataTransferオブジェクトに対して、fileを追加
+      //6
       dataBox.items.add(file)
-      //DataTransferオブジェクトに入ったfile一覧をfile_fieldの中に代入
+      //7
       file_field.files = dataBox.files
 
       var num = $('.post-image').length + 1 + i
       fileReader.readAsDataURL(file);
-     //画像が5枚になったら超えたらドロップボックスを削除する
+     //8
       if (num == 4){
         $('#image-box__container').css('display', 'none')   
       }
-      //読み込みが完了すると、srcにfileのURLを格納
+      //9
       fileReader.onloadend = function() {
         var src = fileReader.result
         var html= `<div class='post-image' data-image="${file.name}">
@@ -92,38 +35,38 @@ $(function(){
                       <div class='post-image__operetion--delete'>削除</div>
                     </div>
                   </div>`
-        //image_box__container要素の前にhtmlを差し込む
+        //10
         $('#image-box__container').before(html);
       };
-      //image-box__containerのクラスを変更し、CSSでドロップボックスの大きさを変えてやる。
+      //11
       $('#image-box__container').attr('class', `post-num-${num}`)
     });
   });
-  //削除ボタンをクリックすると発火するイベント
+  //12
   $(document).on("click", '.post-image__operetion--delete', function(){
-    //プレビュー要素を取得
+    //13
     var target_image = $(this).parent().parent()
-    //削除を押されたプレビューimageのfile名を取得
+    //14
     var target_name = $(target_image).data('image')
     if(file_field.files.length==1){
-      //inputタグに入ったファイルを削除
+      //15
       $('input[type=file]').val(null)
       dataBox.clearData();
       console.log(dataBox)
     }else{
-      //プレビューが複数の場合
+      //16
       $.each(file_field.files, function(i,input){
-        //削除を押された要素と一致した時、index番号に基づいてdataBoxに格納された要素を削除する
+        //17
         if(input.name==target_name){
           dataBox.items.remove(i) 
         }
       })
-      //DataTransferオブジェクトに入ったfile一覧をfile_fieldの中に再度代入
+      //18
       file_field.files = dataBox.files
     }
-  //プレビューを削除
+  //19
   target_image.remove()
-  //image-box__containerクラスをもつdivタグのクラスを削除のたびに変更
+  //
   var num = $('.post-image').length
   $('#image-box__container').show()
   $('#image-box__container').attr('class', `post-num-${num}`)
