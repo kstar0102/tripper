4de48fa4484fd6e9882b9d0aee40118e63c91094
kstar0102 <seniordev52@gmail.com>
2020-04-16 21:25:33 +0900

    Viewの修正

--- app/assets/javascripts/slick.js
+++ app/assets/javascripts/slick.js
@@ -2,8 +2,8 @@ $(function() {
   $('.slider').slick({
       prevArrow:'<i class="fa fa-angle-left arrow arrow-left"></i>',
       nextArrow:'<i class="fa fa-angle-right arrow arrow-right"></i>',
-      dots: true,
-      autoplay: false,
+      dots: false,
+      autoplay: true,
       autoplaySpeed: 4000,
       speed: 800
   });
