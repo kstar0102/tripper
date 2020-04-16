30dd35b6ba6b4d11ee9eb7ae2be9afab2a2d4d2f
kstar0102 <seniordev52@gmail.com>
2020-04-16 17:53:08 +0900

    マイページに投稿の順序を日付順にした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @name = current_user.name
-    @posts = current_user.posts
+    @posts = current_user.posts.order("created_at DESC")
   end
 
   def edit
